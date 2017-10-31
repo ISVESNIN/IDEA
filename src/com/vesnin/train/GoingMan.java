00e1838715a026d755d4df2e6713c6e8e5466de7
IVAN <macro-in@ya.ru>
2017-10-31 10:50:24 +0300

    small refactor

--- src/com/vesnin/train/GoingMan.java
+++ src/com/vesnin/train/GoingMan.java
@@ -3,14 +3,12 @@ package com.vesnin.train;
 public class GoingMan {
     public static void main(String[] args) {
         int step=0;
-        train CurTrain;
-        CurTrain = new train();
-        CurTrain.newTrain();
-
+        Train curTrain = new Train();
+        curTrain.newTrain();
 
         while (step<=100) {
-            CurTrain.SetOnCarLight();
-            CurTrain.GetNextCarLight();
+            curTrain.SetOnCarLight();
+            curTrain.GetNextCarLight();
             step++;
         }
 
